c67ef53da1fd119a05d7731b382a475e1ae925ec
Juan Segura <nachosegura600@gmail.com>
2025-06-03 23:20:43 -0300

    Segundo commit
    s

---
+++ miLibreriaDigital/src/main/java/util/ProcesadorGenerico.java
@@ -0,0 +1,25 @@
+
+
+package util;
+
+
+public class ProcesadorGenerico<T> {
+
+    private T elemento;
+
+    public ProcesadorGenerico(T elementoInicial) {
+        this.elemento = elementoInicial;
+    }
+
+
+    public void procesarElemento() {
+        if (this.elemento != null) {
+            System.out.println("Procesando elemento: " + elemento.toString());
+            System.out.println("Tipo del elemento: " + elemento.getClass().getSimpleName());
+        } else {
+            System.out.println("No hay elemento para procesar (es nulo).");
+        }
+    }
+ {
+    
+}
